ece9e15b063d8937764ed9b159aca7686059a8ba
Zed <21045701+ZedDrebin@users.noreply.github.com>
2024-03-19 23:45:13 -0400

    Update weather.sh
    Rewrite to fix quit bug

--- weather.sh
+++ weather.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
-# Weather bash CLI script fetches weather from wttr.in
-# Usage: Enter a city on the commandlne. This will become the default city.
-#               Press q to Quit or any other key to enter a new city.
 # Get local weather in terminal
+# bash CLI Weather script fetches weather from wttr.in
+# Usage: Run weather.sh or run with a city on the commandlne. Aguments will become the default city.
+#        Press q to Quit or any other key to enter a new city.
 #
 # Function to fetch weather information
 weather() {
-    local location=$1
-    [ -z "$1" ] && echo "Usage: $0 <city>" && curl wttr.in/:help | less && exit 1
-    curl wttr.in/"$location"
+	local location=$1
+	[ -z "$1" ] && echo "Usage: $0 <city>" && curl wttr.in/:help | less && exit 1
+	curl wttr.in/"$location"
+}
+
+cleanup() {
+	clear
+	exit 0
 }
 
 clear
@@ -16,36 +21,41 @@ clear
 default_city=""
 
 if [ "$#" -eq 1 ]; then
-    default_city="$1"
-    weather "$default_city"
-    read -rsp $'Press \'q\' to Quit. Any Other Key to Continue...\n' -n 1 key
-    clear
+	default_city="$1"
+	weather "$default_city"
+	read -rsp $'Press \'q\' to Quit. Any Other Key to Continue...\n' -n 1 key
+	if [[ $key = q ]];then cleanup
+
+	fi
+
 fi
 
 while true; do
-    if [ -z "$default_city" ]; then
-        read -p "Enter city name (q to quit): " city
-    else
-        read -p "Enter city name (default: $default_city, q to quit): " city
-        if [[ $city == "" ]]; then
-            city="$default_city"
-        fi
-    fi
+	if [ -z "$default_city" ]; then
+		read -rp "Enter city name (q to quit): " city
+	else
+		clear
+		read -rp "Enter city name (default: $default_city, q to quit): " city
+		if [[ $city == "" ]]; then
+			city="$default_city"
+		fi
+	fi
 
-    if [[ $city == "q" ]]; then
-        clear
-        exit 0
-    fi
-    clear
+	if [[ $city == "q" ]]; then
 
-    weather "$city"
-    read -rsp $'Press \'q\' to Quit. Press Any Other Key to Continue...\n' -n 1 key
+		cleanup
 
-    clear
+	fi
+	clear
 
-    if [[ $key == "q" ]]; then
-        clear
-        exit 0
-    fi
+	weather "$city"
+	read -rsp $'Press \'q\' to Quit. Press Any Other Key to Continue...\n' -n 1 key
+
+	clear
+
+	if [[ $key == "q" ]]; then
+		clear
+		cleanup
+	fi
 
 done
